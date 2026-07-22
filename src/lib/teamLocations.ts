fd39a52334eb93f624662a6d323c1b45a269ce7a
John Berger <john.berger@wbd.com>
2026-07-22 15:34:54 -0400

    update locations

--- src/lib/teamLocations.ts
+++ src/lib/teamLocations.ts
@@ -9,9 +9,9 @@ export type TeamLocation = {
 export const TEAM_LOCATIONS: TeamLocation[] = [
   { city: 'Los Angeles, CA', latitude: 34.0522, longitude: -118.2437, countryCode: 'US', countryName: 'United States' },
   { city: 'Seattle, WA', latitude: 47.6062, longitude: -122.3321, countryCode: 'US', countryName: 'United States' },
-  { city: 'Detroit, MI', latitude: 42.3314, longitude: -83.0458, countryCode: 'US', countryName: 'United States' },
+  { city: 'Michigan', latitude: 42.737, longitude: -84.4839, countryCode: 'US', countryName: 'United States' },
   { city: 'Atlanta, GA', latitude: 33.749, longitude: -84.388, countryCode: 'US', countryName: 'United States' },
-  { city: 'Upstate New York', latitude: 42.6526, longitude: -73.7562, countryCode: 'US', countryName: 'United States' },
+  { city: 'Westchester County, NY', latitude: 41.2348, longitude: -73.6946, countryCode: 'US', countryName: 'United States' },
   { city: 'Vancouver, BC', latitude: 49.2827, longitude: -123.1207, countryCode: 'CA', countryName: 'Canada' },
   { city: 'Quito, Ecuador', latitude: -0.1807, longitude: -78.4678, countryCode: 'EC', countryName: 'Ecuador' },
 ];
